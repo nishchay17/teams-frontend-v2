8e7dcfdcf534d683998269f1d5b60f2043ea3101
Nishchay <nishchay13971@gmail.com>
2023-07-14 02:41:13 +0530

    added progress bar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,5 @@
 import { Icons } from "@/components/icons";
 import { DashboardNav } from "@/components/nav/dashboard-nav";
-import NavbarWrapper from "@/components/nav/nav-wrapper";
 import { AdminDashboardNav } from "@/config/dashboard-nav";
 
 interface DashboardLayoutProps {
@@ -12,9 +11,21 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
     <>
       <div className="border-b-[1px] mb-6">
         <nav className="mx-auto max-w-[1700px] p-4">
-          <div className="flex gap-3 items-center">
-            <Icons.hero height={24} width={24} />
-            <h1 className="font-semibold text-lg">Teams Collab</h1>
+          <div className="flex justify-between items-center">
+            <div className="flex gap-3 items-center">
+              <Icons.hero height={24} width={24} />
+              <h1 className="font-semibold text-lg">Teams Collab</h1>
+            </div>
+            <div>
+              <p className="text-xs mb-1">
+                <i>Your Status</i>
+              </p>
+              <div className="flex rounded w-[200px] overflow-hidden">
+                <div title="New" className="bg-red-500 w-[20%] h-2" />
+                <div title="In progress" className="bg-sky-500 w-[50%] h-2" />
+                <div title="Completed" className="bg-emerald-500 w-[30%] h-2" />
+              </div>
+            </div>
           </div>
         </nav>
       </div>
