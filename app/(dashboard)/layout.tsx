e458e886a266d9cb3d7222f2498ed7320141ffa6
Nishchay <nishchay13971@gmail.com>
2023-07-14 00:32:31 +0530

    added dashboard layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,31 @@
+import { Icons } from "@/components/icons";
+import { DashboardNav } from "@/components/nav/dashboard-nav";
+import NavbarWrapper from "@/components/nav/nav-wrapper";
+import { AdminDashboardNav } from "@/config/dashboard-nav";
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <>
+      <div className="border-b-[1px] mb-6">
+        <nav className="mx-auto max-w-[1700px] p-4">
+          <div className="flex gap-3 items-center">
+            <Icons.hero height={24} width={24} />
+            <h1 className="font-semibold text-lg">Teams Collab</h1>
+          </div>
+        </nav>
+      </div>
+      <div className="px-4 grid flex-1 gap-12 md:grid-cols-[200px_1fr] mx-auto max-w-[1700px]">
+        <aside className="hidden w-[200px] flex-col md:flex">
+          <DashboardNav items={AdminDashboardNav} />
+        </aside>
+        <main className="flex w-full flex-1 flex-col overflow-hidden">
+          {children}
+        </main>
+      </div>
+    </>
+  );
+}
---
+++ config/dashboard-nav.ts
@@ -0,0 +1,28 @@
+export const AdminDashboardNav = [
+  {
+    title: "Tasks",
+    disabled: false,
+    href: "/dashboard/tasks",
+    icon: "checkBox",
+  },
+  {
+    title: "Add Task",
+    disabled: false,
+    href: "/dashboard/add-task",
+    icon: "add-list",
+  },
+  {
+    title: "Profile",
+    disabled: false,
+    href: "/dashboard",
+    icon: "profile",
+  },
+  { title: "Bucket", disabled: false, href: "/", icon: "file" },
+  { title: "Add User", disabled: false, href: "/", icon: "add" },
+  {
+    title: "Manage User",
+    disabled: false,
+    href: "/",
+    icon: "manage",
+  },
+];
