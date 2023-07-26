8dec16f9323ad2a0a69b9c9470637c2d94d5227e
Nishchay <nishchay13971@gmail.com>
2023-07-26 23:33:39 +0530

    added user card

--- app/(dashboard)/dashboard/manage-users/page.tsx
+++ app/(dashboard)/dashboard/manage-users/page.tsx
@@ -1,5 +1,26 @@
 import { Button } from "@/components/ui/button";
 
+function UserProfileCard({
+  username,
+  total,
+  completed,
+  inprogress,
+}: {
+  username: string;
+  total: number;
+  inprogress: number;
+  completed: number;
+}) {
+  return (
+    <div className="bg-secondary rounded py-3 px-4">
+      <h4 className="mb-2">{username}</h4>
+      <p className="text-sm">Total Tasks: {total}</p>
+      <p className="text-sm">Completed Tasks: {completed}</p>
+      <p className="text-sm">In progress Tasks: {inprogress}</p>
+    </div>
+  );
+}
+
 export default function ManageUsers() {
   return (
     <>
@@ -8,6 +29,44 @@ export default function ManageUsers() {
         <Button form="add-task" type="submit">
           Add User
         </Button>
+      </div>
+      <div className="grid grid-cols-4 gap-4 mt-5">
+        <UserProfileCard
+          username="Nishchay"
+          total={10}
+          completed={6}
+          inprogress={4}
+        />
+        <UserProfileCard
+          username="Nishchay"
+          total={10}
+          completed={6}
+          inprogress={4}
+        />
+        <UserProfileCard
+          username="Nishchay"
+          total={10}
+          completed={6}
+          inprogress={4}
+        />
+        <UserProfileCard
+          username="Nishchay"
+          total={10}
+          completed={6}
+          inprogress={4}
+        />
+        <UserProfileCard
+          username="Nishchay"
+          total={10}
+          completed={6}
+          inprogress={4}
+        />
+        <UserProfileCard
+          username="Nishchay"
+          total={10}
+          completed={6}
+          inprogress={4}
+        />
       </div>
     </>
   );
