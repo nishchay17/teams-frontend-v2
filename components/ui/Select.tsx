0498c04acab695db53d152780353fbda1029afb8
Nishchay <nishchay13971@gmail.com>
2023-08-06 01:44:19 +0530

    added react select

---
+++ components/ui/Select.tsx
@@ -0,0 +1,61 @@
+import clsx from "clsx";
+import { default as S, Props } from "react-select";
+
+export default function Select(props: Props) {
+  const controlStyles = {
+    base: "border rounded-md bg-transparent hover:cursor-pointer text-sm w-full px-3 py-1 shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring",
+    focus: "ring-1 ring-gray-400 dark:ring-gray-800",
+    nonFocus: "border-input",
+  };
+  const placeholderStyles = "text-gray-400";
+  const selectInputStyles = "text-primary";
+  const valueContainerStyles = "p-1 gap-1";
+  const singleValueStyles = "text-primary ml-1";
+  const indicatorsContainerStyles = "p-1 gap-1";
+  const indicatorSeparatorStyles = "bg-input";
+  const dropdownIndicatorStyles =
+    "p-1 hover:bg-gray-100 dark:hover:bg-gray-800 text-gray-500 dark:text-gray-600 rounded-md";
+  const menuStyles =
+    "p-1 mt-2 border border-input bg-transparent rounded-md text-sm";
+  const noOptionsMessageStyles = "text-foreground py-1 text-sm";
+  const optionStyles = {
+    base: "hover:cursor-pointer px-3 py-2 rounded text-primary",
+    focus: "bg-gray-100 dark:bg-gray-900",
+    selected: "bg-gray-200 dark:bg-gray-800",
+  };
+
+  return (
+    <S
+      unstyled
+      {...props}
+      classNames={{
+        control: ({ isFocused }: { isFocused: boolean }) =>
+          clsx(
+            isFocused ? controlStyles.focus : controlStyles.nonFocus,
+            controlStyles.base
+          ),
+        placeholder: () => placeholderStyles,
+        input: () => selectInputStyles,
+        valueContainer: () => valueContainerStyles,
+        singleValue: () => singleValueStyles,
+        indicatorsContainer: () => indicatorsContainerStyles,
+        indicatorSeparator: () => indicatorSeparatorStyles,
+        dropdownIndicator: () => dropdownIndicatorStyles,
+        menu: () => menuStyles,
+        option: ({
+          isFocused,
+          isSelected,
+        }: {
+          isFocused: boolean;
+          isSelected: boolean;
+        }) =>
+          clsx(
+            isFocused && optionStyles.focus,
+            isSelected && optionStyles.selected,
+            optionStyles.base
+          ),
+        noOptionsMessage: () => noOptionsMessageStyles,
+      }}
+    />
+  );
+}
