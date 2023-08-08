445ec4c09b0fc344be9cede08f89f3c904a06451
Nishchay <nishchay13971@gmail.com>
2023-08-08 22:46:24 +0530

    build fix

--- config/dashboard-nav.ts
+++ config/dashboard-nav.ts
@@ -5,10 +5,6 @@ export const AdminDashboardNav = [
   Links.addTask,
   Links.bucket,
   Links.manageUser,
-] as const;
+];
 
-export const UserDashboardNav = [
-  Links.task,
-  Links.addTask,
-  Links.bucket,
-] as const;
+export const UserDashboardNav = [Links.task, Links.addTask, Links.bucket];
