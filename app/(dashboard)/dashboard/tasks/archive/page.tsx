e68f703e936b691e157386632ebc217ce450b4c4
Nishchay <nishchay13971@gmail.com>
2023-08-20 18:36:06 +0530

    added archived task refetch btn

--- app/(dashboard)/dashboard/tasks/archive/page.tsx
+++ app/(dashboard)/dashboard/tasks/archive/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { Icons } from "@/components/icons";
 import TaskCard from "@/components/taskCard";
+import { Button } from "@/components/ui/button";
 import Pagination from "@/components/ui/pagination";
 import { Links } from "@/config/links";
 import useArchiveTasks from "@/hooks/useArchivedTasks";
@@ -31,6 +33,7 @@ export default function ArchiveTask({}: Props) {
   const [pageNo, setPageNo] = useState(0);
   const PER_PAGE = 10;
   const archiveTasks = useArchiveTasks(pageNo, PER_PAGE);
+  const ReloadIcon = Icons["reload"];
 
   function onPageChange({ selected }: { selected: number }) {
     setPageNo(selected);
@@ -53,7 +56,17 @@ export default function ArchiveTask({}: Props) {
 
   return (
     <>
-      <h2 className="text-2xl mb-7">Archived Tasks</h2>
+      <div className="flex justify-between items-center mb-7">
+        <h2 className="text-2xl">Archived Tasks</h2>
+        <Button
+          aria-label="reload tasks"
+          onClick={() => archiveTasks.refetch()}
+          variant="ghost"
+          isLoading={archiveTasks.isFetching}
+        >
+          {!archiveTasks.isFetching && <ReloadIcon size="1rem" />}
+        </Button>
+      </div>
       {archiveTasksList.map((task: any) => (
         <TaskCard
           {...task}
